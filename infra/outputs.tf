f391703743052108cf38cacef2c4c3898afe1bdf
Aramnara <ar4834@nyu.edu>
2025-04-05 20:05:42 -0400

    Terraform deployed: IAM + Lambda setup complete

---
+++ infra/outputs.tf
@@ -0,0 +1,4 @@
+output "lambda_function_name" {
+  value = aws_lambda_function.secure_lambda.function_name
+}
+
---
+++ infra/variables.tf
@@ -0,0 +1,6 @@
+variable "lambda_function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "secure-serverless-lambda"
+}
+
